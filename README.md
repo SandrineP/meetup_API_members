978b90f5078ed36165e213bb7f1204ab812d3651
SandrineP <pataut.sandrine@gmail.com>
2018-01-21 11:40:06 +0000

    now using the waitlist, adding the move_to_main_list function, improvement on score

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 With the other organisers of the AI club for Gender Minorities, we decided not to allow our members to be put directly on the main list as we wanted to prioritise those who are assiduous. So far, I've done that manually. I'm now trying to automate the process to gain time and to avoid human bias by giving a weight to the different situations (attended, no show, last day drop out, last minute excuse, waitlisted). There are things to improve and others that will come later (see comments).
 
-The other thing I'll like to automate is the update of the database (a spreadsheet so far) I use to register the attendance. The problem I have is that the meetup API allows only 200 requests per hour. The temporary solution that I found is to only add those who joined the waitlist.
+The other thing I'll like to automate is the update of the database I use to register the attendance. The problem I have is that the meetup API allows only 200 requests per hour. The temporary solution that I found is to only add those who joined the waitlist.
--- meetup_rsvp.py
+++ meetup_rsvp.py
@@ -24,50 +24,39 @@ weight_W = 0.4
 # capacity of the event
 capacity = int(input("Capacity of the event: "))
 
+def move_to_main_list(ev_id, m_id):
+    client.CreateRsvp(event_id = ev_id, member_id = m_id, rsvp = 'yes')
+
 def to_main_list(event):
 
     if event['waitlist_count'] != 0:
         nb_ml = event['yes_rsvp_count']  # number of people already on the main list
 
         rsvps = client.GetRsvps(event_id = event['id'])     # getting the rsvp
-                   # to do: get the waitlist only
+        w = [rsvps.results[i]['response'] == 'waitlist' for i in range(len(rsvps.results))]
+        waitlist = [m for (m,r) in zip(rsvps.results, w) if r]
                    # to do: ordering by date of answer
 
         if nb_ml < capacity:
-            for m in rsvps.results:
+            for m in waitlist:
                 m_id = m['member']['member_id']
                     # to do: checking if question is answered
 
                 if m_id not in members.index:
                     print(m_id, m['member']['name'])
-                    client.CreateRsvp(event_id = event['id'],
-                                      member_id = m_id,
-                                      rsvp = 'yes')         # moving the member to the mainlist
+                    move_to_main_list(event['id'], m_id)     # moving the member to the mainlist
 
-                    members.loc[m_id] = m['member']['name']      #adding the new member to the database
+                    members.loc[m_id] = m['member']['name']      #adding the new member to database
                     members.loc[m_id, 1:6] = 0
                     members.loc[m_id, 6:] = 'NaN'
 
 
                 else:
-                    if members.loc[m_id, 'N - Nb No Show']
-                     + members.loc[m_id, 'D - Nb last day drop out']
-                     + members.loc[m_id, 'E - Nb last min excuse'] == 0:
-                        client.CreateRsvp(event_id = event['id'],
-                                          member_id = m_id,
-                                          rsvp = 'yes')         # moving the member to the mainlist
+                    s = [w*p for w,p in zip(weight,members.loc[m_id][1:6])]
+                    score = sum(s)
 
-                    else:
-                        score = (weight_A*members.loc[m_id, 'A - Nb attended']
-                                + weight_W*members.loc[m_id, 'W - Nb time wailtlisted'])
-                                / max((weight_N*members.loc[m_id, 'N - Nb No Show']
-                                + weight_D*members.loc[m_id, 'D - Nb last day drop out']
-                                + weight_E*members.loc[m_id, 'E - Nb last min excuse']), 1)
-
-                        if score > 1:
-                            client.CreateRsvp(event_id = event['id'],
-                                              member_id = m_id,
-                                              rsvp = 'yes')         # moving the member to the mainlist
+                    if score >= 0:
+                        move_to_main_list(event['id'], m_id)     # moving the member to the mainlist
 
     # to do: complete the mainlist 24h before the event, whatever the score is
 
@@ -79,4 +68,4 @@ for event in events.results:
     # to do: fill the database with A/N/D/E/W
 
 # saving the updated database
-members.to_csv('AI_club_attendance.csv')    
+members.to_csv('AI_club_attendance.csv')
